240b0095db8517b731ec5b54bfb3c871c4f15137
Joseph Reilly <reilly4181@gmail.com>
2019-04-04 18:07:10 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,7 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(respond)
 if "I LOVE YOU GRANDMA!"
-  puts "I LOVE YOU TOO PUMPKIN!"
+   "I LOVE YOU TOO PUMPKIN!"
   # puts "HUH?! SPEAK UP, SONNY!"
 end
 end

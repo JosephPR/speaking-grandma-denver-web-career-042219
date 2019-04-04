842c2d14ebbd926530eab2c2807acb09c1745cf5
Joseph Reilly <reilly4181@gmail.com>
2019-04-04 18:27:22 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -4,7 +4,7 @@ def speak_to_grandma(phrase)
     return "NO, NOT SINCE 1938!"
     else if
     return "HUH?! SPEAK UP, SONNY!"
-    else phrase == "I LOVE YOU GRANDMA!"
+    else phrase = "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
   end
     end
